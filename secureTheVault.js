93a5d2a9b27d906be01681c751a564447d1e640a
Tor-Skaarva <torskaa@gmail.com>
2025-02-18 19:38:03 -0500

    Add files via upload

---
+++ secureTheVault.js
@@ -0,0 +1,10 @@
+//I will complete this excercise by creating three differnt varriables that through arithmetic solve to 10-40-39. Then log their output together through console commands. Then I will set the combination to display in an alert message when I launch the browser. The folowing is the first varriable.
+let numA = 5 + 5;
+//This is the second varriable
+let numB = 20 + 2 * 10;
+//This is the third varriable
+let numC = 40 - 1;
+//Now I will combine them
+console.log(numA + "-" + numB + "-" + numC);
+//Now I will set the combination to display in an alert message when I launch the browser
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination: " + numA + "-" + numB + "-" + numC + " Don't tell nobody!");
